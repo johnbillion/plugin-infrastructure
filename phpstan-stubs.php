1ab57899a4b9c2c8e21da7787b0b2a800aef9764
John Blackbourn <john@humanmade.com>
2023-03-19 00:03:45 +0100

    Add PHPStan stubs for WP core.

---
+++ phpstan-stubs.php
@@ -0,0 +1,23 @@
+<?php
+
+// WP core constants:
+
+define( 'DB_HOST', '' );
+define( 'DB_NAME', '' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_USER', '' );
+
+define( 'SAVEQUERIES', false );
+define( 'WP_CONTENT_DIR', '' );
+define( 'WP_MEMORY_LIMIT', '' );
+define( 'WP_PLUGIN_DIR', '' );
+define( 'WPINC', '' );
+define( 'WP_DEFAULT_THEME', '' );
+
+define( 'AUTH_COOKIE', '' );
+define( 'COOKIE_DOMAIN', '' );
+define( 'COOKIEHASH', '' );
+define( 'COOKIEPATH', '' );
+define( 'LOGGED_IN_COOKIE', '' );
+define( 'SECURE_AUTH_COOKIE', '' );
+define( 'SITECOOKIEPATH', '' );
